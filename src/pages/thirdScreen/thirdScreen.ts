f62520ce3eb104e1e950118fcb2d4062379267a5
Fredy B <tovar_fredy@hotmail.com>
2017-05-27 20:10:03 -0500

    geo

--- src/pages/thirdScreen/thirdScreen.ts
+++ src/pages/thirdScreen/thirdScreen.ts
@@ -1,19 +1,38 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 
+import { Geolocation } from '@ionic-native/geolocation';
+
 @Component({
   selector: 'page-third',
-  templateUrl: 'thirdScreen.html'
+  templateUrl: 'thirdScreen.html',
+  providers: [Geolocation]
 })
 export class thirdScreenPage {
   selectedItem: any;
 
   constructor(public navCtrl: NavController, 
-              public navParams: NavParams) {
+              public navParams: NavParams,
+              private geolocation: Geolocation ) {
     // If we navigated to this page, we will have an item available as a nav param
     this.selectedItem = navParams.get('item');
     // Let's populate this page with some filler content for funzies
 
   }
+
+  getGeo(){
+    this.geolocation.getCurrentPosition().then((resp) => {
+ // resp.coords.latitude
+ // resp.coords.longitude
+      console.log(resp.coords.latitude,resp.coords.longitude) 
+    }).catch((error) => {
+      console.log('Error getting location', error);
+    });
+  }
+
+  sendInfo() {
+    this.getGeo()
+  }
+
 
 }
