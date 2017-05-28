89b43cde1d814cab1e577fc569007e8cba94d344
Camilo Arguello <camilo.arguello.rincon@gmail.com>
2017-05-28 06:31:00 -0500

    Sent data from front to tomcat (backend) ok

--- src/pages/thirdScreen/thirdScreen.ts
+++ src/pages/thirdScreen/thirdScreen.ts
@@ -28,18 +28,19 @@ export class thirdScreenPage {
       }
 
       var xmlhttp = new XMLHttpRequest();
-      var url = "http://ec2-52-15-226-254.us-east-2.compute.amazonaws.com:8080/helpym/webresources/denuncias/";
+      var url = "http://ec2-52-15-226-254.us-east-2.compute.amazonaws.com:8080/helpym/webresources/denuncias/put";
 
       xmlhttp.onreadystatechange = function() {
       if (this.readyState == 4 && this.status == 200) {
           var myArr = JSON.parse(this.responseText);
           console.log(myArr);
-      }
-    };
+          alert("mensaje enviado")
+        }
+      };
 
-      xmlhttp.open("GET", url, true);
-      xmlhttp.send();
-
+      xmlhttp.open("PUT", url, true);
+      xmlhttp.setRequestHeader("Content-Type", "application/json");
+      xmlhttp.send(JSON.stringify(myJson));
 
     }).catch((error) => {
       console.log('Error getting location', error);
